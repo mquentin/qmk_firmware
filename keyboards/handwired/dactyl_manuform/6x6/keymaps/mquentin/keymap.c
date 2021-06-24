7493989412db2ddce950d46e22ea8d4d8fc731f8
maxime.quentin <maxime.quentin@datadoghq.com>
2021-06-24 12:58:40 +0200

    add flash cmd

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/mquentin/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/mquentin/keymap.c
@@ -20,6 +20,7 @@
 
 
 // To compile: qmk compile -kb handwired/dactyl_manuform/6x6 -km mquentin
+// To flash: qmk flash -kb handwired/dactyl_manuform/6x6 -km mquentin
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE]=LAYOUT_6x6(
