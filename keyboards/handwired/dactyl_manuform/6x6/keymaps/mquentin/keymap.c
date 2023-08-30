cf58d88939f2971ab2fdd1d2069471d2c7aa60d5
maxime.quentin <maxime.quentin@datadoghq.com>
2023-08-30 12:13:33 +0200

    merge and fix conflicts

--- keyboards/handwired/dactyl_manuform/6x6/keymaps/mquentin/keymap.c
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/mquentin/keymap.c
@@ -7,6 +7,8 @@
 
 #define ESC_RSTA LT(RESET_ACCESS, KC_ESC)
 
+#define RESET__ QK_BOOT
+
 // empty layer
 // [RESET]=LAYOUT_6x6(
 //   _______, _______,   _______, _______, _______, _______,                _______, _______, _______, _______, _______, _______,
@@ -28,12 +30,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE]=LAYOUT_6x6(
     ESC_RSTA,    KC_F1,   KC_F2,    KC_F3,    KC_F4,    KC_F5,              KC_F6,  KC_F7,  KC_F8,    KC_F9,   KC_F10,  FR_EQL,
     FR_SUP2,     FR_AMPR, FR_EACU,  FR_DQUO,  FR_QUOT,  FR_LPRN,            FR_MINS,FR_EGRV,FR_UNDS,  FR_CCED, FR_AGRV, FR_RPRN,
-    KC_CAPSLOCK, FR_A,    FR_Z,     FR_E,     FR_R,     FR_T,               FR_Y,   FR_U,   FR_I,     FR_O,    FR_P,    FR_CIRC,
+    KC_CAPS_LOCK, FR_A,    FR_Z,     FR_E,     FR_R,     FR_T,               FR_Y,   FR_U,   FR_I,     FR_O,    FR_P,    FR_CIRC,
     KC_F12,      FR_Q,    FR_S,     FR_D,     FR_F,     FR_G,               FR_H,   FR_J,   FR_K,     FR_L,    FR_M,    FR_UGRV,
     KC_F11,      FR_W,    FR_X,     FR_C,     FR_V,     FR_B,               FR_N,   FR_COMM,FR_SCLN,  FR_COLN, FR_DLR,  FR_ASTR,
                             FR_LABK,  KC_TAB,                                               KC_RALT,    FR_EXLM,
-                                                KC_SPACE,  KC_LGUI,         KC_BSPACE,  KC_ENTER,
-                                                  KC_LCTRL, KC_LSHIFT,           KC_UP,     KC_RIGHT,
+                                                KC_SPACE,  KC_LGUI,         KC_BSPC,  KC_ENTER,
+                                                  KC_LCTL, KC_LSFT,           KC_UP,     KC_RIGHT,
                                                   KC_LALT,  KC_ENTER,           KC_LEFT, KC_DOWN
   ),
   [RESET_ACCESS]=LAYOUT_6x6(
@@ -54,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______,         _______, _______, _______, _______, _______, _______,
                       _______, _______,                                             _______, _______,
-                                  _______, RESET,                     _______, _______,
+                                  _______, RESET__,                     _______, _______,
                                         _______, _______,         _______, _______,
                                         _______, _______,         _______, _______
   ),
