5c2e02c20e6d7446c913b4c1335ab41f5a717f01
maxime.quentin <maxime.quentin@datadoghq.com>
2024-09-17 13:29:45 +0200

    switch to sym_eager_pk

--- keyboards/handwired/dactyl_manuform/6x6/rules.mk
+++ keyboards/handwired/dactyl_manuform/6x6/rules.mk
@@ -1,3 +1,5 @@
-DEBOUNCE_TYPE = asym_eager_defer_pk
+# https://docs.qmk.fm/feature_debounce_type#types-of-debounce-algorithms
+# DEBOUNCE_TYPE = asym_eager_defer_pk
+DEBOUNCE_TYPE = sym_eager_pk
 
 DEFAULT_FOLDER = handwired/dactyl_manuform/6x6/promicro
